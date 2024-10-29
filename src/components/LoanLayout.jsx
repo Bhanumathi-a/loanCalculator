a16207543766761d4079797581a75da0c63cc754
Bhanumathi-a <61862519+Bhanumathi-a@users.noreply.github.com>
2024-10-29 19:09:40 +0530

    Add files via upload
    fixed some issues

--- src/components/LoanLayout.jsx
+++ src/components/LoanLayout.jsx
@@ -16,20 +16,27 @@ const LoanLayout = () => {
   let totalInterest = monthlyPayment * loanTenure - loanAmount
   totalInterest = Math.round(totalInterest)
 
-  let totalAmount = loanAmount + totalInterest
+  // let totalAmount = `${loanAmount}  ${totalInterest}`
+  let totalAmount = parseInt(loanAmount) + parseInt(totalInterest)
 
   // console.log(monthlyInterestRate, monthlyPayment)
 
   return (
     <>
-      <div className='container p-12 m-auto w-2/3'>
+      <div className='container p-12 m-auto w-full lg:w-2/3'>
         <h2 className='text-2xl text-center font-sans font-semibold text-blue-900'>
           Calculate Loan
         </h2>
+        <p className='block text-lg font-sans pt-4 pb-6'>
+          Use our Loan Calculator to get insights on your loan plan! Just select
+          an amount, set an approximate interest rate and loan tenure between 12
+          and 360 months. The Loan EMI calculator will estimate the monthly EMI
+          amount and total Interest payable till the end of the loan tenure.
+        </p>
         <div className='relative mb-8'>
           <label
             htmlFor='loanAmt'
-            className='block text-xl font-sans leading-6 pb-5 '>
+            className='block text-xl font-sans leading-6 pb-7 '>
             Loan Amount
           </label>
           <input
@@ -38,14 +45,14 @@ const LoanLayout = () => {
             min='100000'
             max='10000000'
             step='10'
-            className='w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700'
+            className='w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700 accent-blue-900'
             value={loanAmount}
             onChange={(e) => setLoanAmount(e.target.value)}
           />
           <span
-            className={`bg-blue-900 -mt-8 text-white truncate text-sm rounded py-1 px-4 absolute bottom-4 `}
+            className={`bg-blue-900 -mt-8 text-white truncate text-sm rounded py-1 px-4 absolute bottom-5 `}
             style={{ left: 0 }}>
-            {/* ${loanAmount / 100000} */}₹{loanAmount}
+            ₹{loanAmount}
           </span>
 
           <span className='text-sm text-gray-500 dark:text-gray-400 absolute start-0 -bottom-6'>
@@ -56,10 +63,10 @@ const LoanLayout = () => {
             ₹ 10000000
           </span>
         </div>
-        <div className='grid grid-cols-2 gap-5 mb-10'>
+        <div className='grid grid-cols-1 gap-5 mb-10 lg:grid-cols-2'>
           <div className='relative mb-8 w-full inline-block'>
             <label
-              className='block text-xl font-sans leading-6 pb-5 '
+              className='block text-xl font-sans leading-6 pb-7 '
               htmlFor='interest'>
               Interest Rate (%)
             </label>
@@ -69,12 +76,12 @@ const LoanLayout = () => {
               name='Interest'
               min='5'
               max='20'
-              className='w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700'
+              className='w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700 accent-blue-900'
               value={loanInterest}
               onChange={(e) => setLoanInterest(e.target.value)}
             />
             <span
-              className={`bg-blue-900 -mt-8 text-white truncate text-sm rounded py-1 px-4 absolute bottom-4 `}
+              className={`bg-blue-900 -mt-8 text-white truncate text-sm rounded py-1 px-4 absolute bottom-5 `}
               style={{ left: 0 }}>
               {loanInterest}
             </span>
@@ -88,7 +95,7 @@ const LoanLayout = () => {
           </div>
           <div className='relative mb-8 w-full inline-block'>
             <label
-              className='block text-xl font-sans leading-6 pb-5 '
+              className='block text-xl font-sans leading-6 pb-7 '
               htmlFor='tenure'>
               Tenure (In months)
             </label>
@@ -98,12 +105,12 @@ const LoanLayout = () => {
               name='Tenure'
               min='12'
               max='360'
-              className='w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700'
+              className='w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700 accent-blue-900'
               value={loanTenure}
               onChange={(e) => setLoanTenure(e.target.value)}
             />
             <span
-              className={`bg-blue-900 -mt-8 text-white truncate text-sm rounded py-1 px-4 absolute bottom-4 `}
+              className={`bg-blue-900 -mt-8 text-white truncate text-sm rounded py-1 px-4 absolute bottom-5 `}
               style={{ left: 0 }}>
               {loanTenure}
             </span>
@@ -116,20 +123,20 @@ const LoanLayout = () => {
             </span>
           </div>
         </div>
-        <div id='result' className='grid grid-cols-2 gap-5 mb-2'>
+        <div id='result' className='grid grid-cols-1 gap-5 mb-2 lg:grid-cols-2'>
           <div>
             <h3 className='block text-xl font-sans leading-6 '>
               Principal Amount
             </h3>
-            <p className='block text-2xl font-semibold font-sans leading-6 '>
+            <p className='block text-2xl font-semibold font-sans leading-6  text-blue-900'>
               ₹{loanAmount}
             </p>
           </div>
           <div>
-            <h3 className='block text-xl font-sans leading-6 '>
+            <h3 className='block text-xl font-sans leading-6'>
               Monthly EMI Amount
             </h3>
-            <p className='block text-2xl font-semibold font-sans leading-6 '>
+            <p className='block text-2xl font-semibold font-sans leading-6  text-blue-900'>
               ₹{monthlyPayment}
             </p>
           </div>
@@ -137,7 +144,7 @@ const LoanLayout = () => {
             <h3 className='block text-xl font-sans leading-6 '>
               Interest Payable
             </h3>
-            <p className='block text-2xl font-semibold font-sans leading-6 '>
+            <p className='block text-2xl font-semibold font-sans leading-6  text-blue-900'>
               ₹{totalInterest}
             </p>
           </div>
@@ -145,11 +152,18 @@ const LoanLayout = () => {
             <h3 className='block text-xl font-sans leading-6 '>
               Total Amount Payable
             </h3>
-            <p className='block text-2xl font-semibold font-sans leading-6 '>
+            <p className='block text-2xl font-semibold font-sans leading-6  text-blue-900'>
               ₹{totalAmount}
             </p>
           </div>
         </div>
+        <p className='block text-sm font-sans text-gray-700 pt-4'>
+          <strong>Disclaimer*</strong>: Our Loan EMI calculator offers estimated
+          monthly instalments which are indicative and tentative and are based
+          upon the details populated by the user. Actual loan terms and
+          eligibility are subject to bank approval. For precise loan details,
+          consult our representatives before decisions based on these estimates.
+        </p>
       </div>
     </>
   )
